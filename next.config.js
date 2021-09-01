4bf16815f02bd414521a8a3fa110c3cf550990b0
Rendy Artha <rendyartha312@gmail.com>
2021-09-01 15:02:13 +0700

    fixing logging workbox

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
-const withPWA = require("next-pwa");
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
   reactStrictMode: true,
   pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
+    dest: 'public',
+    scope: '/',
+    runtimeCaching,
+    disable: process.env.NODE_ENV === 'development',
   },
   images: {
     domains: ['raw.githubusercontent.com'],
