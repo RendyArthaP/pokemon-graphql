4de4d54afd5fc3fae3bb1635a3c8a8cf387c8240
Rendy Artha <rendyartha312@gmail.com>
2021-09-01 16:49:35 +0700

    setup service worker, and improve performance and pwa

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
   reactStrictMode: true,
   pwa: {
     dest: 'public',
-    scope: '/',
-    runtimeCaching,
-    disable: process.env.NODE_ENV === 'development',
+    disable: process.env.NODE_ENV === 'development'
   },
   images: {
     domains: ['raw.githubusercontent.com'],
